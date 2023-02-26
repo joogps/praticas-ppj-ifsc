767ea2a70f2ff185b61528e2b5bae11aaf655eb5
João Gabriel <joogps@gmail.com>
2023-02-26 02:14:31 -0300

    Adicionar enumeração de planetas

--- Sistema Solar/Assets/Scripts/RotateAround.cs
+++ Sistema Solar/Assets/Scripts/RotateAround.cs
@@ -5,6 +5,7 @@ public class RotateAround : MonoBehaviour {
 
 	public Transform target; // the object to rotate around
 	public float speed; // the speed of rotation
+	public Planets planet; // the speed of rotation
 	
 	void Start() {
 		if (target == null) 
@@ -22,3 +23,40 @@ public class RotateAround : MonoBehaviour {
 		transform.RotateAround(target.transform.position,target.transform.up,speed * Time.deltaTime);
 	}
 }
+
+public enum Planets
+{
+	Mercury,
+	Venus,
+	Earth,
+	Mars,
+	Jupiter,
+	Saturn,
+	Uranus,
+	Neptune
+}
+
+// public int DaysInYear(Planets planet)
+// {
+// 	switch(planet)
+// 	{
+// 		case Planets.Mercury:
+// 			return 88;
+// 		case Planets.Venus:
+// 			return 225;
+// 		case Planets.Earth:
+// 			return 365;
+// 		case Planets.Mars:
+// 			return 687;
+// 		case Planets.Jupiter:
+// 			return 4333;
+// 		case Planets.Saturn:
+// 			return 10759;
+// 		case Planets.Uranus:
+// 			return 30685;
+// 		case Planets.Neptune:
+// 			return 60190;
+// 		default:
+// 			return 0;
+// 	}
+// }
